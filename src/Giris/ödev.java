4e295bb5909a0d081021a7dc890ee027685c1238
rooiboss <72911438+rooiboss@users.noreply.github.com>
2022-03-15 16:23:33 +0300

    First commit

--- src/Giris/ödev.java
+++ src/Giris/ödev.java
@@ -4,35 +4,17 @@ import java.util.Scanner;
 
 public class ödev {
     public static void main(String[] args) {
-    int mat , fizik , kimya , türkçe , müzik , tarih ;
-        Scanner inp= new Scanner(System.in);
+        System.out.print("Kdvsi Hesaplanacak Tutarı Giriniz :");
+        Scanner scan = new Scanner(System.in);
 
-        System.out.print("Matematik Notunuz:");
-        mat = inp.nextInt();
-
-
-        System.out.print("Tarih Notunuz:");
-        tarih = inp.nextInt();
-
-
-        System.out.print("Türkçe Notunuz:");
-        türkçe = inp.nextInt();
-
-
-        System.out.print("Kimya Notunuz:");
-        kimya = inp.nextInt();
-
-
-        System.out.print("Fizik Notunuz:");
-        fizik = inp.nextInt();
-
-
-        System.out.print("Müzik Notunuz:");
-        müzik = inp.nextInt();
-
-        int toplam = (mat + fizik + müzik + kimya + tarih + türkçe);
-        double sonuc =(toplam/6);
-        System.out.println(sonuc);
+        double amount = scan.nextDouble();
+        double rateVate = 0.18;
+        double rateVate2 = 0.08;
+        double kdvTutarı =  amount > 0 && amount <=1000 ? amount * rateVate  : amount * rateVate2 ;
+        double kdvliTutar =  amount > 0 && amount <=1000 ? amount * rateVate +amount : amount * rateVate2 +amount;
+        System.out.println("KdvliTutar :" + kdvliTutar);
+        System.out.println("Kdv'siz Fiyat :" + amount);
+        System.out.println("KDV tutarı :" + kdvTutarı);
 
 
 
